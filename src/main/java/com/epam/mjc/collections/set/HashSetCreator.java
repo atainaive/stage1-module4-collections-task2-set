00330023d9094db05513eaf6728cacf702f2924f
atainaive <atai.omorov@stud.vgtu.lt>
2022-11-27 13:13:07 +0200

    done

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (Integer i : sourceList) {
+            if (i % 2 != 0) {
+                hashSet.add(i);
+                hashSet.add(i * 2);
+            } else {
+                while (i % 2 == 0) {
+                    hashSet.add(i);
+                    i /= 2;
+                }
+                hashSet.add(i);
+            }
+        }
+
+        return hashSet;
     }
 }
